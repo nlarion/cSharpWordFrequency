2daa2e1586d8b9b46aab9465454706d46aad7445
Neil Larion <neil.larion@gmail.com>
2016-02-26 16:20:21 -0800

    Fix Tests and plain english specs to match requirements of code review

--- Tests/WFTest.cs
+++ Tests/WFTest.cs
@@ -8,39 +8,75 @@ namespace  WordFrequency
   public class RepeatCounterTest
   {
     [Fact]
-    public void TestRepeatCounter()
+    public void Test_RepeatCounter_MatchSimpleWordInJustOneWord()
+    {
+      Assert.Equal(1, RepeatCounter.CountRepeats("world","world"));
+    }
+
+    [Fact]
+    public void Test_RepeatCounter_MatchSimpleSentenceInTwoWords()
     {
       Assert.Equal(1, RepeatCounter.CountRepeats("Hello world","world"));
     }
 
     [Fact]
-    public void TestRepeatCounterForEmpty()
+    public void Test_RepeatCounter_MatchSimpleSentenceInThreeWords()
+    {
+      Assert.Equal(3, RepeatCounter.CountRepeats("world world world","world"));
+    }
+
+    [Fact]
+    public void Test_RepeatCounter_MatchSimpleSentenceWithACapitalLetter()
+    {
+      Assert.Equal(1, RepeatCounter.CountRepeats("Hello World","world"));
+    }
+
+    [Fact]
+    public void Test_RepeatCounter_MatchSimpleSentenceWithSpecialCharacter()
+    {
+      Assert.Equal(1, RepeatCounter.CountRepeats("Hello world!","world"));
+    }
+
+    [Fact]
+    public void Test_RepeatCounter_MatchSimpleSentenceWithSpecialCharacterAndCapitalLetter()
+    {
+      Assert.Equal(1, RepeatCounter.CountRepeats("Hello World!","world"));
+    }
+
+    [Fact]
+    public void Test_RepeatCounter_NoMatchWhenMatchingWordIsApartOfAnotherWord()
+    {
+      Assert.Equal(0, RepeatCounter.CountRepeats("HelloWorld!","world"));
+    }
+
+    [Fact]
+    public void Test_RepeatCounter_MatchComplexLongSentence()
+    {
+      Assert.Equal(2, RepeatCounter.CountRepeats("Hello world! This is a complex sentence with multiple things to try and trick the counter. Like this: helloworld. What a WORLD.","world"));
+    }
+
+    [Fact]
+    public void Test_RepeatCounter_MatchWehnInputWordHasACapital()
+    {
+      Assert.Equal(1, RepeatCounter.CountRepeats("Hello world!","World"));
+    }
+
+    [Fact]
+    public void Test_RepeatCounter_ForEmptyMatchWord()
+    {
+      Assert.Equal(0, RepeatCounter.CountRepeats("Hello World",""));
+    }    
+
+    [Fact]
+    public void Test_RepeatCounter_ForEmptySentence()
+    {
+      Assert.Equal(0, RepeatCounter.CountRepeats("","world"));
+    }    
+
+    [Fact]
+    public void Test_RepeatCounter_ForEmptyMatchWordAndSentence()
     {
       Assert.Equal(0, RepeatCounter.CountRepeats("",""));
-    }
-
-    [Fact]
-    public void TestRepeatCounterLongTest()
-    {
-      Assert.Equal(2, RepeatCounter.CountRepeats("Hello world! This is a complex sentence with multiple things to try and trick the counter. What a WORLD.","world"));
-    }
-    
-    [Fact]
-    public void TestReturnLowerCharArray()
-    {
-      Assert.Equal(new char[]{'w','o','r','l','d'}, RepeatCounter.LowerCharArrayFromString("WORLD"));
-    }
-
-    [Fact]
-    public void TestReturnListOfWordsFromSentence()
-    {
-      Assert.Equal(new List<string>{"This","is","a","split","sentence"}, RepeatCounter.ListOfWordsFromSentence("This is a split sentence"));
-    }
-
-    [Fact]
-    public void TestReturnTrueWhenCharArraysAreEqual()
-    {
-      Assert.Equal(true, RepeatCounter.TrueWhenCharArraysAreEqual(new char[]{'y','e','s'},new char[]{'y','e','s'}));
     }
   }
 }
